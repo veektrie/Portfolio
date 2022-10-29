fdd33f996fbcd8f89532619c865a0142f1911e00
veektrie <102025110+veektrie@users.noreply.github.com>
2022-10-29 16:54:52 +0100

    Higher order component implemented

--- src/container/About/about.jsx
+++ src/container/About/about.jsx
@@ -1,40 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
 
 import "./About.scss";
+import { urlFor, client } from "../../client";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description:
-      "I am a Web Developer with 2 years of experience, I also have a comprehensive understanding of common programming paradigms and fundamental React principles, such as React components, hooks, and the React lifecycle, and experience in designing and implementing user interfaces (UIs) and UI components using Bootstrap and Tailwind, I have Developed and maintained UIs for web and mobile apps meeting standards for web design, user experience, best practices, and speed.",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Backend Development",
-    description:
-      "A thorough grasp of MySQL and Sanity.io in contributing to generating custom Schemas for specific organizational needs.",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Digital Marketing",
-    description:
-      "I also lend detailed knowledge of Digital Marketing to connect with potential customers using the internet and other forms of digital communication. This includes using tools that are at a company's disposal. These channels include social media, the company's own website, search engine rankings/optimization, email marketing and display advertising.",
-    imgUrl: images.about03,
-  },
-  {
-    title: "Web Design",
-    description:
-      "I have Developed and maintained UIs for web and mobile apps meeting standards for web design, user experience, best practices, and speed.I bring a strong commitment to collaboration and solutions-oriented problem-solving. Use various web design packages to develop custom-crafted,customer-focused websites and designs.",
-    imgUrl: images.about04,
-  },
-];
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Design</span> <br />
+        I Know that <span>Good Development</span> <br />
         means <span>Good Business</span>
       </h2>
 
@@ -47,7 +32,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
@@ -61,4 +46,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, "about");
--- src/container/Header/header.jsx
+++ src/container/Header/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
 import "./header.scss";
 
 const scaleVariants = {
@@ -33,7 +34,7 @@ const Header = () => {
 
           <div className="tag-cmp app__flex">
             <p className="p-text">Web Developer</p>
-            <p className="p-text">Freelancer</p>
+            <p className="p-text">Digital Marketer</p>
           </div>
         </div>
       </motion.div>
@@ -67,4 +68,4 @@ const Header = () => {
     </div>
   );
 };
-export default Header;
+export default AppWrap(Header, "home");
