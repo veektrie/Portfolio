7aaa7b4f1c336d2ac96e4dc358e3d262af494b46
Adugbo Victory <102025110+veektrie@users.noreply.github.com>
2022-11-01 16:21:03 +0100

    Update index.js

--- src/container/index.js
+++ src/container/index.js
@@ -1,4 +1,4 @@
-export { default as About } from './About/About';
+export { default as About } from './About/about';
 export { default as Footer } from './Footer/footer';
 export { default as Header } from './Header/header';
 export { default as Skills } from './Skills/skills';
