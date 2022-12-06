61c3b94848efe5d053559a4ac6d8f2c406a2412d
veektrie <102025110+veektrie@users.noreply.github.com>
2022-12-06 07:53:52 +0100

    final changes

--- src/container/Header/header.jsx
+++ src/container/Header/header.jsx
@@ -59,13 +59,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[
-          images.react,
-          images.javascript,
-          images.sass,
-          images.html,
-          images.git,
-        ].map((circle, index) => (
+        {[images.react, images.sass, images.git].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="profile_bg" />
           </div>
